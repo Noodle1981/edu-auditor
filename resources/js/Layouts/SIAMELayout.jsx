a0b9c51be29bd54a446304b1c49ec7d6613b7fb2
Noodle1981 <omardigital81@gmail.com>
2026-06-12 13:30:22 -0300

    fix: wrap Inertia app with GlobalProvider at the root level to fix context error and persist state

--- resources/js/Layouts/SIAMELayout.jsx
+++ resources/js/Layouts/SIAMELayout.jsx
@@ -2,29 +2,26 @@ import React from 'react';
 import { Sidebar } from '../Components/Sidebar';
 import { Header } from '../Components/Header';
 import { AgentModal } from '../Components/AgentModal';
-import { GlobalProvider } from '../Context/GlobalContext';
 
 export default function SIAMELayout({ children }) {
   return (
-    <GlobalProvider>
-      <div className="min-h-screen flex bg-gray-50/20">
-        {/* Fixed Sidebar */}
-        <Sidebar />
+    <div className="min-h-screen flex bg-gray-50/20">
+      {/* Fixed Sidebar */}
+      <Sidebar />
 
-        {/* Main Container */}
-        <div className="flex-1 flex flex-col min-w-0">
-          {/* Top Header */}
-          <Header />
+      {/* Main Container */}
+      <div className="flex-1 flex flex-col min-w-0">
+        {/* Top Header */}
+        <Header />
 
-          {/* Dynamic Route Content */}
-          <main className="flex-1 p-10 max-w-[1400px] w-full mx-auto">
-            {children}
-          </main>
-        </div>
-
-        {/* Global Agent Modal */}
-        <AgentModal />
+        {/* Dynamic Route Content */}
+        <main className="flex-1 p-10 max-w-[1400px] w-full mx-auto">
+          {children}
+        </main>
       </div>
-    </GlobalProvider>
+
+      {/* Global Agent Modal */}
+      <AgentModal />
+    </div>
   );
 }
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -5,6 +5,8 @@ import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
+import { GlobalProvider } from './Context/GlobalContext';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -17,7 +19,11 @@ createInertiaApp({
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(<App {...props} />);
+        root.render(
+            <GlobalProvider>
+                <App {...props} />
+            </GlobalProvider>
+        );
     },
     progress: {
         color: '#FE8204',
